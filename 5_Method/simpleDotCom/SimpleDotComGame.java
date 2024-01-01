8d38f888384f51d2f6d306a20b1c2c0bdbd51f12
coi618 <coi618@hotmail.com>
2024-01-01 13:49:43 +0800

    SimpleDotCom v1 Finish

--- 5_Method/simpleDotCom/SimpleDotComGame.java
+++ 5_Method/simpleDotCom/SimpleDotComGame.java
@@ -2,7 +2,33 @@
 
 public class SimpleDotComGame {
 	public static void main(String[] args) {
-		
+		// Count how many time player guess
+		int numOfGuesses = 0;
+		// New a game helper
+		GameHelper helper = new GameHelper();
+		// New a DotCom
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		// Set DotCom's location [1-5]
+		int randomNum = (int)(Math.random()*5);
+		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		theDotCom.setLocationCells(locations);
+		// Set DotCom alive
+		boolean isAlive = true;
+		// Play game: non-stop if DotCom still alive
+		while(isAlive) {
+			// Get user guess from helper
+			String guess = helper.getUserInput("Enter a number: ");
+			// check guess
+			String result = theDotCom.checkYourself(guess);
+			// Guess number add 1
+			numOfGuesses++;
+			// if kill DotCom, make isAlive=false
+			if(result.equals("kill")) {
+				isAlive = false;
+				// print number of guess
+				System.out.println("You took "+numOfGuesses+" guesses.");
+			} // end if 
+		} // end while
 	}
 }
 
---
+++ 5_Method/simpleDotCom/mySimpleDotComGame.java
@@ -0,0 +1,48 @@
+// Filename: mySimpleDotComGame.java
+import java.io.*;
+
+public class mySimpleDotComGame {
+	public static void main(String[] args) {
+		// Start Game
+		// : Initial the environment
+		// 1. Build a DotCom
+		mySimpleDotCom dotCom = new mySimpleDotCom();
+		// 2. Set DotCom location
+		dotCom.setLocationCells((int)(Math.random()*5)); //[0-4]
+		// 3. Set DotCom as alive
+		boolean Alive = true;
+		// 4. Set a guess counter = 0
+		int guessCount = 0;
+
+		// TODO: Play Game(loop: guess > check > response)
+		// 1. Loop if DotCom still alive
+		while(Alive) {
+		// 2. Declare result = "miss" / guess counter +1
+			String result = "miss";
+			guessCount++;
+		// 3. Ask player to enter the guess
+			String userGuess= null;
+			System.out.print("Enter your guess: ");
+			try {
+				BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+				userGuess = is.readLine();
+				if(userGuess.length() == 0) userGuess = null;
+			} catch (IOException e) {
+				System.out.println("IOException: "+e);
+			} // end try..catch
+		// 4. Print func return result
+			result = dotCom.checkYourself(userGuess);
+			System.out.println(result);
+
+		// 5. Check if kill(No: Continue / Yes: Set alive=false)
+			if(result == "kill") Alive = false;
+		} // end while
+
+		// TODO: End the Game
+		// 1. Print how many time user guess
+		System.out.println("You kill DotCom with "+guessCount+" guess!!");
+		//*2. Say goodbye to user
+		System.out.println("Thanks for play.");
+
+	} // end main
+} // end class
